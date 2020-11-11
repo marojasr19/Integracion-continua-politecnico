15e954e7d8eb15a787657872e195790d0a6edf04
technologicalsolutions <serviciotec@technologicalsolutions.page>
2020-11-10 21:15:46 -0500

    Rene Paramo: Ajustes contenido repositorio

--- holamundo/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-def inicio(request):
-    template = loader.get_template('holamundo/inicio.html')
-    context = {
-        
-    }
-    return HttpResponse(template.render(context, request))
